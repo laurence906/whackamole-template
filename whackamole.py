dce2b4fc0b5f6613cc7bbf7a23e5a7627b283621
Laurence <laurenceschrade@gmail.com>
2024-11-15 16:15:40 -0500

    added grid lines

--- whackamole.py
+++ whackamole.py
@@ -15,6 +15,10 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            for i in range(1,20):
+                pygame.draw.line(screen, (0,50,255), (i * 32, 0), (i * 32, 512), 1) #draw columns
+            for i in range(1,16):
+                pygame.draw.line(screen, (0,50,255), (0, i * 32), (640, i * 32), 1)
             pygame.display.flip()
             clock.tick(60)
     finally:
